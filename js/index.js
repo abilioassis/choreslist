bd55af82cdabf84597463726a0b50b65ffa32f9b
Abilio de Assis <abilio.assis@petrobras.com.br>
2023-05-10 10:23:12 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
 "use strict";
+
+/**
+ * Event handlers
+ */
+document.getElementById("chore-add").addEventListener("click", function () {
+  choreAdd();
+});
+
+document
+  .getElementById("chore-remove-all")
+  .addEventListener("click", function () {
+    choreRemoveAll();
+  });
+
+initializeApp();
+
+function initializeApp() {
+  for (let idx = 0; idx < localStorage.length; idx++) {
+    const key = localStorage.key(idx);
+    const chore = localStorage.getItem(key);
+    updateUI("chore", chore);
+  }
+}
+
+function choreAdd() {
+  const choreInput = document.getElementById("chore-input").value;
+
+  if (choreInput !== "") {
+    if (localStorage.getItem(choreInput) !== null) {
+      // chore exists in localStorage
+      updateUI("warning", "This task already exists.");
+    } else {
+      // chore doesn't exists in localStorage
+      // const key = Date.now().toString();
+      updateUI("warning", "");
+      localStorage.setItem(choreInput, choreInput);
+      updateUI("chore", choreInput);
+    }
+  }
+}
+
+function choreRemoveAll() {
+  localStorage.clear();
+  updateUI("clear", "");
+}
+
+function updateUI(strType, str) {
+  const msgEl = document.getElementById("msg");
+  const choreInputEl = document.getElementById("chore-input");
+  const choreListEl = document.getElementById("chores-list");
+
+  if (strType === "warning") {
+    msgEl.innerText = str;
+  } else if (strType === "chore") {
+    choreInputEl.value = "";
+    choreListEl.innerHTML += `<div class="chore">${str}</div>`;
+  } else if (strType === "clear") {
+    choreListEl.innerHTML = str;
+  }
+}
